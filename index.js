03371449673dffe04e93103f91447e8e79a0318e
Fernando <fernanguedezr@gmail.com>
2026-02-04 22:02:51 +0100

    fix: Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,26 @@ const PORT = 3000;
 const EXTERNAL_API = "https://api-books-ac3j.onrender.com";
 
 // middleware
+app.use(cors());
+app.use(express.json());
+
+// rutas
+app.get("/users", async (req, res) => {
+  try {
+    const response = await axios.get(`${EXTERNAL_API}/users`);
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener usuarios" });
+  }
+});
+
+app.get("/books", async (req, res) => {
+  try {
+    const response = await axios.get(`${EXTERNAL_API}/books`);
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener libros" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
